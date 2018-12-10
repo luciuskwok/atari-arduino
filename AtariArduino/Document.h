c346c83d22d50afa0a7765241222eb9b4a81d617
Lucius Kwok <lk@felttip.com>
2018-12-10 01:26:44 -0500

    Initial Commit

---
+++ AtariArduino/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  AtariArduino
+//
+//  Created by Lucius Kwok on 12/10/18.
+//  Copyright © 2018 Lucius Kwok. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
---
+++ AtariArduino/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AtariArduino
+//
+//  Created by Lucius Kwok on 12/10/18.
+//  Copyright © 2018 Lucius Kwok. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
