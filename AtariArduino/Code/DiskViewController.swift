504832c5491f564f5fabb48c9260883405fc04bc
Lucius Kwok <lk@felttip.com>
2018-12-14 02:33:36 -0500

    Simplified code for detecting option key.

--- AtariArduino/Code/DiskViewController.swift
+++ AtariArduino/Code/DiskViewController.swift
@@ -156,12 +156,10 @@ class DiskViewController: NSViewController, NSTableViewDelegate, NSTableViewData
 		
 		//NSLog("[LK] Validate drop.")
 		
-		// TODO: Unless the option key is held down, dragging a file within the same disk image should do nothing
+		// Unless the option key is held down, dragging a file within the same disk image should do nothing
 		if info.draggingSource as? NSTableView == tableView {
-			if let event = NSApp.currentEvent {
-				if event.modifierFlags.contains(.option) == false {
-					return []
-				}
+			if let event = NSApp.currentEvent, event.modifierFlags.contains(.option) == false {
+				return []
 			}
 		}
 		
